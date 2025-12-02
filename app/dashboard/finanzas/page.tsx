9909e16a9941462236c32596ab607dc7f6ff3936
Sebastián  Castro <sebastiancastro@192.168.1.19>
2025-12-02 00:46:17 -0300

    Fix finanzas page

--- app/dashboard/finanzas/page.tsx
+++ app/dashboard/finanzas/page.tsx
@@ -1,7 +1,7 @@
 export default function FinanzasPage() {
   return (
-    <div>
-      <h1>Finanzas</h1>
+    <div className="p-6">
+      <h1 className="text-3xl font-semibold">Finanzas</h1>
     </div>
   );
 }
