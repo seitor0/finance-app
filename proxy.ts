84dc146062c76273f08caf978a5d8c745a130b6c
Sebastián Castro <sebastiancastro.ok@gmail.com>
2025-12-06 09:51:45 -0300

    Unificación de estilos + nuevas páginas de tarjetas

--- proxy.ts
+++ proxy.ts
@@ -1,18 +1,16 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const token = req.cookies.get("authToken")?.value;
   const { pathname } = req.nextUrl;
 
   const isAuthPage = pathname.startsWith("/login");
 
-  // Si no hay token y trata de entrar a dashboard
   if (!token && pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  // Si hay token y está en /login → dashboard
   if (token && isAuthPage) {
     return NextResponse.redirect(new URL("/dashboard", req.url));
   }
